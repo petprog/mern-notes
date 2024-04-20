b35928550e48b7beb0f309337caf422d4791890c
petprog <farinupeter@gmail.com>
2024-04-20 12:23:15 +0100

    fix auth slice

--- client/src/features/auth/authApiSlice.js
+++ client/src/features/auth/authApiSlice.js
@@ -1,28 +1,27 @@
 import { apiSlice } from "../../app/api/apiSlice";
-import { logOut } from "./authSlice";
+import { logOut, setCredentials } from "./authSlice";
 
 export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    login: builder.query({
+    login: builder.mutation({
       query: (credentials) => ({
         url: "auth",
         method: "POST",
         body: credentials,
       }),
-      validateStatus: (response) => {
-        return response.status === 200;
-      },
     }),
     sendLogout: builder.mutation({
       query: () => ({
         url: "auth/logout",
         method: "POST",
       }),
-      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
           await queryFulfilled;
           dispatch(logOut());
-          dispatch(apiSlice.util.resetApiState());
+          setTimeout(() => {
+            dispatch(apiSlice.util.resetApiState());
+          }, 1000);
         } catch (err) {
           console.log(err);
         }
@@ -33,9 +32,18 @@ export const authApiSlice = apiSlice.injectEndpoints({
         url: "auth/refresh",
         method: "GET",
       }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          const { accessToken } = data;
+          dispatch(setCredentials({ accessToken }));
+        } catch (err) {
+          console.log(err);
+        }
+      },
     }),
   }),
 });
 
-export const { useLoginQuery, useSendLogoutMutation, useRefreshMutation } =
+export const { useLoginMutation, useSendLogoutMutation, useRefreshMutation } =
   authApiSlice;
