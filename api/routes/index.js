8767854ab5d08f5c66d64bc79463359d8185eb56
petprog <farinupeter@gmail.com>
2024-04-23 18:52:18 +0100

    remove root routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import usersRouter from "./user.route.js";
-import rootRouter from "./root.route.js";
+// import rootRouter from "./root.route.js";
 import notesRouter from "./note.route.js";
 import authRouter from "./auth.route.js";
 
@@ -9,6 +9,6 @@ const router = Router();
 router.use("/api/user", usersRouter);
 router.use("/api/note", notesRouter);
 router.use("/api/auth", authRouter);
-router.use("/", rootRouter);
+// router.use("/", rootRouter);
 
 export default router;
